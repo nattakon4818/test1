ceb566e02fe9933eced74c957d330c3fb6a7da27
nine <132211467+nattakon4818@users.noreply.github.com>
2025-07-08 13:37:57 +0700

    Update Exercise3-1.py

--- project/python/งานอ.สมชัย/บทที่ 3/Exercise3-1.py
+++ project/python/งานอ.สมชัย/บทที่ 3/Exercise3-1.py
@@ -10,9 +10,6 @@ money %= 500
 b100 = money // 100
 money %= 100
 
-if b1000 > 0:
-    print(f"Cash B1000 : {b1000:.1f}")
-if b500 > 0:
-    print(f"Cash B500  : {b500:.1f}")
-if b100 > 0:
-    print(f"Cash B100  : {b100:.1f}")
+print(f"Cash B1000 : {b1000:.1f}")
+print(f"Cash B500  : {b500:.1f}")
+print(f"Cash B100  : {b100:.1f}")
